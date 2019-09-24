19ba21f93de319efe89c4466f9ec3a1b70062b96
bcc <shiblyxp@yahoo.com>
2019-09-24 10:53:48 +0200

    tutorial13-03

--- src/app/home/info-box/info-box.component.ts
+++ src/app/home/info-box/info-box.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChange
+} from '@angular/core';
 
 @Component({
   selector: 'app-info-box',
   templateUrl: './info-box.component.html',
   styleUrls: ['./info-box.component.scss']
 })
-export class InfoBoxComponent implements OnInit {
+export class InfoBoxComponent implements OnInit, OnChanges {
   private _name: string;
 
   @Input()
@@ -20,7 +26,23 @@ export class InfoBoxComponent implements OnInit {
     return this._name;
   }
 
-  constructor() {}
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: Record<string, SimpleChange>) {
+    if (changes.message) {
+      console.log('changes.message', changes.message.currentValue);
+    }
+
+    if (changes.name) {
+      console.log('changes.name', changes.name.currentValue);
+    }
+
+    if (changes.message && changes.name) {
+      console.log('Message AND Name changed');
+    }
+  }
 }
